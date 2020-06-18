e4b7c23daf77b2e2c7d81368098365ebe89d7f5a
Mikaela <mikaela301@gmail.com>
2020-06-17 22:56:30 -0700

    Create views folder, list.ejs file, and ejs template

--- app.js
+++ app.js
@@ -3,14 +3,23 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use("view engine", "ejs");
+app.use("view engine", "ejs")
 
 app.get("/", function(req, res){
   
     var today = new Date();
     var currentDay = today.getDay();
+    var day = "";
 
-})
+    if (currentDay === 6 || currentDay === 0) {
+        day = "Weekend";
+    } else {
+        day = "Weekday";
+    }
+
+    res.render("list", {kindOfDay: day});
+    
+});
 
 
 app.listen(8080, function(){
