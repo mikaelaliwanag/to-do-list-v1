729a572dfac153af73178c9795cfcb8b0d1e3d84
Mikaela <mikaela301@gmail.com>
2020-06-21 16:24:53 -0700

    Add post request

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const app = express();
 
 app.set("view engine", "ejs");
 
+app.use(bodyParser.urlencoded({extended: true}))
+
 app.get("/", function (req, res) {
 
 	var today = new Date();
@@ -19,6 +21,11 @@ app.get("/", function (req, res) {
 
     res.render("list", {kindOfDay: day });
 });
+
+app.post("/", function(req, res) {
+	console.log(req.body.newItem);
+})
+
 
 app.listen(3000, function () {
 	console.log("Server started on port 3000"); 
