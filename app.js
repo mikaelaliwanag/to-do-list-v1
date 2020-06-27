0ffd1824521217248ae65ec071f542ae0154fc08
mikaelaliwanag <mikaela301@gmail.com>
2020-06-27 16:05:57 -0700

    Create date.js. Use node module exports.

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
 //to do list items array
-let items = ["Code", "Nap", "Code some more"];
+const items = ["Code", "Nap", "Code some more"];
 
 app.set("view engine", "ejs");
 
@@ -13,22 +14,14 @@ app.use(express.static("public"));
 
 app.get("/", function (req, res) {
 
-	let today = new Date();
-
-	let options = {
-		weekday:"long",
-		day: "numeric",
-		month: "long"
-	};
-
-	let day = today.toLocaleDateString("en-us", options);
+	const day = date.getDate();
 
     res.render("list", {kindOfDay: day, newListItems: items});
 });
 
 app.post("/", function(req, res) {
 	
-	let item = req.body.newItem;
+	const item = req.body.newItem;
 
 //does not push item if input is empty	
 	if(item !== "") {
